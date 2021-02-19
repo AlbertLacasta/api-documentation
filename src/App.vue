2ba8d7989382df7e0f107b1735f7b0d586ac6cb2
Albert Lacasta <47864341+AlbertLacasta@users.noreply.github.com>
2021-02-19 16:14:34 +0100

    init

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <v-app>
+        <v-main>
+            <router-view />
+        </v-main>
+    </v-app>
+</template>
+
+<script lang="ts">
+
+import {
+    defineComponent,
+} from "@vue/composition-api";
+
+export default defineComponent({
+    name: "App",
+    setup() {
+
+        return {
+        };
+    }
+});
+</script>
